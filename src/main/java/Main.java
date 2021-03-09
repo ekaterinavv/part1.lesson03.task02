990224fea782def459d372c1c506b2b7268071ec
ekaterina <ekaterinavv@bk.ru>
2021-03-09 19:17:17 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        double q, k;
+        long qRound;
+        System.out.println("Input n:");
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int[] arr = new int[n];
+
+        //Генерация N случайных чисел.
+        Random random = new Random();
+        for (int i = 0; i < n; i++)
+            arr[i] = random.nextInt(100) - 50;
+
+        //Для каждого числа k вычислить квадратный корень q.
+        // Если квадрат целой части q числа равен k, то вывести это число на экран.
+        // Предусмотреть что первоначальные числа могут быть отрицательные, в этом случае генерировать исключение.
+        for (int j : arr) {
+            k = j;
+
+            if (k < 0) {
+                try {
+                    throw new Exception();
+                } catch (Exception ex) {
+                    System.out.println("k = " + k + " is negative number");
+                    continue;
+                }
+
+            }
+            q = Math.sqrt(k);
+            qRound = Math.round(Math.sqrt(k));
+            if (k == qRound * qRound) {
+                System.out.println("k = " + k + ", q = " + q);
+            }
+        }
+    }
+}
